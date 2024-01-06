d600cac27d1b0b56f02e551ae59840a8804dd808
vuhungthang <vuhungthang2707@gmail.com>
2024-01-06 15:40:26 -0600

    added content

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# TTeazy website
+# TTEasy
+
---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {appStyle} from './AppStyles';
+
+export default function App() {
+  return (
+    <div className={appStyle}>
+      <p>
+        Test 1
+      </p>
+
+      <div className="testing">
+        Test 2
+      </div>
+    </div>
+  );
+}
